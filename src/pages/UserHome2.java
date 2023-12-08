ef1a2e019cae46520ea8e68386694dc0f70f6e26
Vaishnav Puram <puramv307@gmail.com>
2023-12-08 20:48:46 +0530

    user home page

--- src/pages/UserHome2.java
+++ src/pages/UserHome2.java
@@ -1,11 +1,16 @@
 package pages;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pojos.User;
 
 /**
  * Servlet implementation class UserHome2
@@ -17,8 +22,14 @@ public class UserHome2 extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		HttpSession session=request.getSession();
+			try(PrintWriter pw=response.getWriter()){
+				pw.println("<h3>Already casted your vote !</h3>");
+				pw.println("<a href=\"login.html\">Signout ?</a>");
+			}
 	}
 
 }

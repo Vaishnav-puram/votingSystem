d3f87da26849679a060b6f6d37c5a0d97f1f7b2c
Vaishnav Puram <puramv307@gmail.com>
2023-12-08 20:48:25 +0530

    displaying user vote status

--- src/pages/UserHome.java
+++ src/pages/UserHome.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.CandidateDaoImpl;
 import dao.UserDaoImpl;
 import pojos.User;
 
@@ -20,9 +21,11 @@ import pojos.User;
 @WebServlet("/userHome")
 public class UserHome extends HttpServlet {
 	UserDaoImpl userDaoImpl;
+	CandidateDaoImpl candidateDaoImpl;
 	public void init(ServletConfig config) throws ServletException {
 		try {
 			userDaoImpl=new UserDaoImpl();
+			candidateDaoImpl=new CandidateDaoImpl();
 		}catch(Exception e) {
 			System.out.print(e.getMessage());
 		}
@@ -43,10 +46,14 @@ public class UserHome extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.setContentType("text/html");
 		try(PrintWriter pw=response.getWriter()){
 			HttpSession session=request.getSession();
 			User u=(User) session.getAttribute("user_info");
+			
 			pw.write("Hello "+u.getFirstName()+" "+u.getLastName()+" your voting status : "+u.isVotingStatus());
+			pw.println();
+			pw.print("<a href=\"castVote.html\">Click here to vote</a>");
 		}
 	}
 
